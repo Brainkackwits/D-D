24f279e902db170fcb82d6ce36730f8c6960d05f
Brainkackwits <two-t.tv@gmx.de>
2021-03-30 16:06:58 +0200

    password login / register

--- user.py
+++ user.py
@@ -8,7 +8,6 @@ def hash_password(password):
     pwdhash = binascii.hexlify(pwdhash)
     return (salt + pwdhash).decode('ascii')
 def verify_password(stored_password, provided_password):
-    """Verify a stored password against one provided by user"""
     salt = stored_password[:64]
     stored_password = stored_password[64:]
     pwdhash = hashlib.pbkdf2_hmac('sha512',
